074e8246b457ad7fa67168104ab7f7fae8195b79
fasahath <syedafasahath@gmail.com>
2020-09-19 16:24:19 -0400

    adding the cities data

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "c56011b0019be593a185868d13207f8d"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyDkb3mzu_6WE2lwTvY2nwH1-LanhGy7fmk"
